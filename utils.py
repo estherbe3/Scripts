140a575d5eff5219a3d9050fb0bee876ce163211
mvdebolskiy <80036033+mvdebolskiy@users.noreply.github.com>
2024-10-02 19:20:13 +0200

    Added a layout for plotting things with depth

--- utils.py
+++ utils.py
@@ -35,7 +35,7 @@ def trans_z(ds,varname,col,depth,timevar):
 
 def subset_vars(hist_dir,out_file,vars=None):
     if vars is None:
-        vars=['EXCESS_ICE','DZSOI','ZSOI','TSOI','SUBSIDENCE','SUBSACC', 'WATSAT', 'SNOW_DEPTH', 'FCH4']
+        vars=['EXCESS_ICE','DZSOI','ZSOI','TSOI','SUBSIDENCE','SUBSACC', 'WATSAT', 'SNOW_DEPTH', 'FCH4','TBOT']
     flpth=glob(hist_dir + '/*h0.*.nc')
     ds = xr.open_mfdataset(flpth,decode_times=True)
     varlist=list(ds.variables.keys())
@@ -46,3 +46,13 @@ def subset_vars(hist_dir,out_file,vars=None):
             
     ds_out=ds[vars]
     ds_out.to_netcdf(path=out_file,mode='a',format='NETCDF4',compute=True)
+
+
+# this one makes ticks with srf at surface, but I would not recommend it.
+def make_yticks(ylim,z_srf,space):
+    yticks=np.concatenate((np.append(np.arange(ylim[0],z_srf,-space),z_srf),np.arange(np.ceil(z_srf/space)*space,ylim[1],-space)))
+    i_ticks=np.where((abs(yticks-z_srf)>space/3) | (yticks==z_srf))[0]
+    yticks=yticks[i_ticks]
+    ytick_lbl=[str(x) for x in yticks]
+    ytick_lbl[np.where(yticks == z_srf)[0][0]]='srf'
+    return yticks, ytick_lbl
